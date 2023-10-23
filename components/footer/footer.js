378ab7c9a518e1cd13c05c8193b36c8af145b069
Ítalo O Costa <italo.de.oliveira01@gmail.com>
2023-10-23 18:38:31 -0300

    ajeitou o footer

---
+++ components/footer/footer.js
@@ -0,0 +1,5 @@
+document.getElementById("footer").innerHTML =
+  "<footer class='footer'>" +
+  "     <div>Desenvolvido por Bianca Évila</div>" +
+  "     <div>Copyright &copy; 2023</div>" +
+  "</footer>";
